d246bb08b87cb69e6fc29694d364ff21a768f69b
Guri Sandhu <gurisandhu@Guri-MacBook-Pro.local>
2017-02-16 11:37:36 +1100

    Template version 1

--- header.php
+++ header.php
@@ -2,9 +2,37 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>BAE</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
+	<title>Flightpath | BAE</title>
 	<link rel="shortcut icon" href="compressed/images/fevicon.ico" type="image/x-icon">
-	<link rel="icon" href="compressed/images/fevicon.ico" type="image/x-icon">
-	<link rel="stylesheet" href="style.css">
+	<link rel="icon" href="compressed/images/favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="library/swiper.min.css" type="text/css" charset="utf-8" />
+	<link rel="stylesheet" href="style.css" type="text/css" charset="utf-8" />
 </head>
 <body>
+<!-- <header class="top">
+	<div class="container"></div>
+</header> -->
+<header>
+	<div class="container">
+		<div class="desktop-header">
+			<div class="logo">
+				<img src="compressed/images/logo.png" alt="">
+			</div>
+			<div class="desktop-menu">
+				<nav>
+					<ul>
+						<li><a href=""></a></li>
+						<li><a href=""></a></li>
+						<li><a href=""></a></li>
+						<li><a href=""></a></li>
+						<li><a href=""></a></li>
+						<li><a href="style-guide.php">Style</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</div>
+</header>
