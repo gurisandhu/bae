529052c12c39d3b7d67c1af02735e7386121b5e5
Guri Sandhu <gurisandhu@Guri-MacBook-Pro.local>
2017-02-20 11:20:53 +1100

    wp added

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ function errorLog(error) {
 }
 gulp.task('serve', ['styles'], function(){
 	browserSync.init({
-		proxy: "http://localhost:8888/bae/"
+		proxy: "http://localhost:8888/baewp/"
 	});
 	gulp.watch('compressed/*.css').on('change', browserSync.reload);
 	gulp.watch('compressed/*.js').on('change', browserSync.reload);
